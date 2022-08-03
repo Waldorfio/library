0f2d94684a0fa87f0a2901ed30da288534638abf
Waldorfio <walid.hanifi707@gmail.com>
2022-08-03 20:04:20 +1000

    Tidy layout, refactor code

--- books.js
+++ books.js
@@ -6,13 +6,13 @@ function Books(title,author,pages,read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.progress = function() {
-      progress = ''
-      if ((pages-read) == 0) {progress = 'finished'}
-      if ((pages-read) != 0) {progress = 'in progress'}
-      if ((pages-read) == pages) {progress = 'not read yet'}
-    return progress
-    }
+    // this.progress = function() {
+    //   progress = ''
+    //   if ((pages-read) == 0) {progress = 'finished'}
+    //   if ((pages-read) != 0) {progress = 'in progress'}
+    //   if ((pages-read) == pages) {progress = 'not read yet'}
+    // return progress
+    // }
 }
 
 // Function to add books to constructor
@@ -21,62 +21,51 @@ function addBookToLibrary(title,author,pages,read) {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary('got0','grrm0',144,true);
-addBookToLibrary('got1','grrm1',244,false);
-addBookToLibrary('got2','grrm2',344,true);
-addBookToLibrary('got3','grrm3',444,false);
-addBookToLibrary('got4','grrm4',544,true);
-
-// Enables form interactivity
-// inputTitle = document.querySelector('#title-input').value
-// inputAuthor = document.querySelector('#author-input').value
-// inputPage = document.querySelector('#pages-input').value
-// inputRead = document.querySelector('#read-input').value
-
-// Creates new books
-// for (i=0; i<(myLibrary.length-1); i++) {
-
-  // inputTitle = myLibrary[i].title;
-  // inputAuthor = myLibrary[i].Author;
-  // inputPage = myLibrary[i].pages;
-  // inputRead = myLibrary[i].read;
-
+// Extract form data
 submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', () => addToPage());
 
 function addToPage() {
+  // Grabs input values from form //
   inputTitle = document.getElementById('title-input').value
   inputAuthor = document.getElementById('author-input').value
   inputPage = document.getElementById('pages-input').value
-  inputRead = true;
-  console.log(inputTitle,inputAuthor,inputPage,inputRead);
+  inputRead = document.getElementById('read-input').value
 
+  addBookToLibrary(inputTitle,inputAuthor,inputPage,inputRead);   // Append book to library array
+
+  // Add a new card class to the content-container parent
   cards = document.createElement('div');
   cards.className = 'cards';
   document.getElementById('content-container').appendChild(cards);
 
-  listOfCards = document.getElementsByClassName('cards');
+  listOfCards = document.getElementsByClassName('cards');         // Get the latest list of cards (to append children to later)
 
+  // Create a new card-title element, within the card class
   cardTitle = document.createElement('div');
   cardTitle.className = 'card-title';
   cardTitle.innerHTML = inputTitle;
   listOfCards[listOfCards.length - 1].appendChild(cardTitle);
 
+  // Create a new card-author element, within the card class
   cardAuthor = document.createElement('div');
   cardAuthor.className = 'card-author';
   cardAuthor.innerHTML = inputAuthor;
   listOfCards[listOfCards.length - 1].appendChild(cardAuthor);
 
+  // Create a new card-page element, within the card class
   cardPage = document.createElement('div');
   cardPage.className = 'card-page';
   cardPage.innerHTML = inputPage;
   listOfCards[listOfCards.length - 1].appendChild(cardPage);
 
+  // Create a new card-read element, within the card class
   cardRead = document.createElement('div');
   cardRead.className = 'card-read';
   cardRead.innerHTML = inputRead;
   listOfCards[listOfCards.length - 1].appendChild(cardRead);
 
+  // Create a new card-button element, within the card class
   cardButton = document.createElement('button');
   cardButton.className = 'card-button';
   cardButton.innerHTML = 'Remove';
